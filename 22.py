e74b034377457a0571b7e2eb14106da50c79e44e
Anton2999 <90238088+Anton2999@users.noreply.github.com>
2021-10-02 13:19:16 +0300

    Add files via upload
    домашка 1

---
+++ 22.py
@@ -0,0 +1,9 @@
+guses1 = int(input('Введите целое число 1 : '))
+guses2 = int(input('Введите целое число 2 : '))
+if guses1>guses2:
+    print('первре число больше второго')
+if guses1<guses2:
+    print('второе число больше первого')   
+if guses1==guses2:
+    print('оба числа равны')
+    
