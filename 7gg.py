0727cdc55ad3842269d3ca0648d36d3ee2b6e8ed
Anton2999 <90238088+Anton2999@users.noreply.github.com>
2021-10-18 23:29:13 +0300

    Update 7gg.py

--- 7gg.py
+++ 7gg.py
@@ -1,8 +1,8 @@
-X=int(input('Введите число X'))
-Y=int(input('Введите число Y'))
-a=range(X,Y+1)
-c=0
+X = int(input('Введите число X'))
+Y = int(input('Введите число Y'))
+a = range(X, Y+1)
+c = 0
 for X in a:
-  if X%5==0:
-   c=X+c
-print(c) 
+    if X % 5 == 0:
+        c = X + c
+print(c)
