7b151f3792cb8ffaeae76846bd6fd171dd4e2005
Anton2999 <90238088+Anton2999@users.noreply.github.com>
2021-10-18 23:19:58 +0300

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,9 +1,8 @@
 guses1 = int(input('Введите целое число 1 : '))
 guses2 = int(input('Введите целое число 2 : '))
-if guses1>guses2:
+if guses1 > guses2:
     print('первре число больше второго')
-if guses1<guses2:
-    print('второе число больше первого')   
-if guses1==guses2:
+if guses1 < guses2:
+    print('второе число больше первого')
+if guses1 == guses2:
     print('оба числа равны')
-    
