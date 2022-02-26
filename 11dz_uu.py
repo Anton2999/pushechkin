aa8fc5687e587fec5692a922114deead0e9138ec
Anton2999 <90238088+Anton2999@users.noreply.github.com>
2022-02-26 09:10:08 +0300

    Update 11dz_uu.py

--- 11dz_uu.py
+++ 11dz_uu.py
@@ -1,24 +1,24 @@
-class Fraction: 
-        def __init__(self, top=0, bottom=1): 
-            self.num = top 
-            self.den = bottom 
+class Fraction:
+        def __init__(self, top=0, bottom=1):
+            self.num = top
+            self.den = bottom
 
         def input(self): 
-            self.num = int(input("Введите числитель: "))         
-            self.den = int(input("Введите знаменатель: "))  
-            if self.den == 0: 
-                print ("error") 
+            self.num = int(input("Введите числитель: "))
+            self.den = int(input("Введите знаменатель: "))
+            if self.den == 0:
+                print ("error")
                 
                 
-        def __str__(self): 
+        def __str__(self):
             return f"{self.num}/{self.den}"
 
-a1 = Fraction() 
-print(a1) 
+a1 = Fraction()
+print(a1)
 
-a2 = Fraction(3, 4) 
-print(a2) 
+a2 = Fraction(3, 4)
+print(a2)
 
-a3 = Fraction() 
-a3.input() 
+a3 = Fraction()
+a3.input()
 print(a3)
