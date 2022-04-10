84f24059a4713224f2117f7869fe4218273d5fbf
Anton2999 <90238088+Anton2999@users.noreply.github.com>
2022-04-10 21:54:48 +0300

    Update test_Fraction.py

--- test_Fraction.py
+++ test_Fraction.py
@@ -4,3 +4,14 @@ def test_str():
 g = Fraction(1, 2)
 assert str(g) == '1/2'
 
+def  test_reduce():
+    f =  Fraction(4,2)
+    f.reduce()
+    assert f == Fraction(2,1)
+  
+def test_inner(moker):
+    moker.patch('builtins.input' , side_effect=['5', '10'])
+    f = IrreduceableFraction()
+    f.inner()
+    assert f == IrreduceableFraction(1, 2)
+      
